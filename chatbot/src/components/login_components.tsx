56826e82860516dd684c6cec1be878f8ba6525be
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2025-08-09 20:00:08 +0530

    added login mobile view and fading of police image

--- chatbot/src/components/login_components.tsx
+++ chatbot/src/components/login_components.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import hello from '../assets/hello.png';
 import angry from '../assets/angry.png';
 import no from '../assets/no.png';
@@ -7,18 +7,88 @@ import eat from '../assets/eat.png';
 import full from '../assets/full.png';
 
 const LoginComp: React.FC = () => {
+    const helloPoliceImage = useRef<HTMLSpanElement>(null);
+    const eatPoliceImage = useRef<HTMLSpanElement>(null);
+    const fullPoliceImage = useRef<HTMLSpanElement>(null);
+    const angryPoliceImage = useRef<HTMLSpanElement>(null);
+    const denyPoliceImage = useRef<HTMLSpanElement>(null);
+    const okPoliceImage = useRef<HTMLSpanElement>(null);
+    const loginContainer = useRef<HTMLDivElement>(null);
+    const calmPoliceDiv = useRef<HTMLDivElement>(null);
+    const angryPoliceDiv = useRef<HTMLDivElement>(null);
+    const happyPoliceDiv = useRef<HTMLDivElement>(null);
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const [imgNum, setImageNum] = useState<number>(5);
+
+    function fadeOut(el: HTMLElement | null) {
+        if (!el) return;
+        el.classList.remove("show");
+    }
+
+    function fadeIn(el: HTMLElement | null) {
+        if (!el) return;
+        el.classList.add("show");
+    }
+
+    useEffect(() => {
+        if (!loggedIn) {
+
+            const timeout = setTimeout(() => {
+                slider();
+            }, imgNum == 5? 100:5000);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [imgNum, loggedIn]);
+
+    function slider() {
+        fadeOut(calmPoliceDiv.current);
+        setTimeout(() => {
+            setImageNum(prev => {
+                const nextNum = (prev + 1) % 3;
+                switch (nextNum) {
+                    case 0:
+                        if (helloPoliceImage.current) helloPoliceImage.current.style.display = "block";
+                        if (eatPoliceImage.current) eatPoliceImage.current.style.display = "none";
+                        if (fullPoliceImage.current) fullPoliceImage.current.style.display = "none";
+                        break;
+                    case 1:
+                        if (helloPoliceImage.current) helloPoliceImage.current.style.display = "none";
+                        if (eatPoliceImage.current) eatPoliceImage.current.style.display = "block";
+                        if (fullPoliceImage.current) fullPoliceImage.current.style.display = "none";
+                        break;
+                    case 2:
+                        if (helloPoliceImage.current) helloPoliceImage.current.style.display = "none";
+                        if (eatPoliceImage.current) eatPoliceImage.current.style.display = "none";
+                        if (fullPoliceImage.current) fullPoliceImage.current.style.display = "block";
+                        break;
+                    default:
+                        if (helloPoliceImage.current) helloPoliceImage.current.style.display = "block";
+                        break;
+                }
+                fadeIn(calmPoliceDiv.current);
+                return nextNum;
+            });
+        }, 1000);
+    }
+
     return (
         <>
-            <div id="loginParent">
+            <div id="loginParent" ref={loginContainer}>
                 <div id="containerOne">
                     <div className="compartment-1 compartment">
                         <div id="policeDiv">
-                            <div className='intro-img-div intro-img-slides'>
-                                <span className="police"><img src={hello} alt="" /></span>
+                            <div className='intro-img-div intro-img-slides' ref={calmPoliceDiv}>
+                                <span className="police police-hello" ref={helloPoliceImage}><img src={hello} alt="police" /></span>
+                                <span className="police police-eat" ref={eatPoliceImage}><img src={eat} alt="police" /></span>
+                                <span className="police police-full" ref={fullPoliceImage}><img src={full} alt="police" /></span>
                             </div>
-                            <div className="intro-img-div error-img-div">
-                                <span className="police police-angry"><img src={hello} alt="" /></span>
-                                <span className="police police-deny"><img src={hello} alt="" /></span>
+                            <div className="intro-img-div error-img-div" ref={angryPoliceDiv}>
+                                <span className="police police-angry" ref={angryPoliceImage}><img src={angry} alt="police" /></span>
+                                <span className="police police-deny" ref={denyPoliceImage}><img src={no} alt="police" /></span>
+                            </div>
+                            <div className="intro-img-div ok-img-div" ref={happyPoliceDiv}>
+                                <span className="police police-ok" ref={okPoliceImage}><img src={ok} alt="police" /></span>
                             </div>
                         </div>
                     </div>
